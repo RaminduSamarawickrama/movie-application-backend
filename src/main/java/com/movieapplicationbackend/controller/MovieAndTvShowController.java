e2581753b7cfea74a4e6a8e47eb1ef842414b641
Ramindu Samarawickrama <ramindu.samarawickrama@gmail.com>
2021-12-16 07:28:22 +0000

    Committed direct into master, Working State CORS annotations added.

--- src/main/java/com/movieapplicationbackend/controller/MovieAndTvShowController.java
+++ src/main/java/com/movieapplicationbackend/controller/MovieAndTvShowController.java
@@ -30,32 +30,42 @@ public class MovieAndTvShowController {
     @Autowired
     TvShowService tvShowService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/getshowsbyname")
     public List<MovieTvShowDTO> searchMovieAndTvShowByName(@RequestParam(value = "name", required = true) String name){
         return showService.getAllShowsByName(name);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/getmoviedetails")
     public MovieDTO getMovieDetails(@RequestParam(value = "id", required = true) int id){
         return movieService.getMovieDetails(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/gettvshowdetails")
     public TvShowDTO getTvShowDetails(@RequestParam(value = "id", required = true) int id){
         return tvShowService.getTvShowDetailsById(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/topten")
     public TrendingShowsDTO getTopTenShows(){
         return showService.getTrendingMoviesAndTvShows();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/dailydumptv")
     public void dailyDumpTv(){
         tmdbDailyDumpService.tvShowDump();
     }
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/dailydumpmovie")
     public void dailyDumpMovie(){
         tmdbDailyDumpService.movieDump();
     }
 }
+
+// localhost:8080/api/getshowsbyname?name=bro
+// localhost:8080/api/getshowsbyname?name=bro
